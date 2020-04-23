34fe23bf439d27cea36654b838f5dd6cb17737a6
Andon Andonov <andon.andonov85@gmail.com>
2020-04-23 19:05:48 +0300

    Added stats

--- src/components/DurationPickerChronometer.vue
+++ src/components/DurationPickerChronometer.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="dp-chronometer__container">
         <div v-on:click="startTimer">start</div>
+        <div>{{ startTime }}</div>
         <div v-on:click="stopTimer">stop</div>
+        <div>{{ stopTime }}</div>
         <div v-on:click="resetTimer">reset</div>
+        <div>total: {{ totalTime }}</div>
 	</div>
 </template>
 
@@ -16,9 +19,9 @@ export default {
     name: "DurationPickerChronometer",
     data: function () {
         return {
-            // interval: store.getCustomInterval('chronometer'),
-            // startTime: null,
-            // stopTime: null
+            startTime: '-',
+            stopTime: '-',
+            totalTime: '-'
         };
     },
     // beforeDestroy: function() {
@@ -30,12 +33,14 @@ export default {
             if (!interval) {
                 let startMilliseconds = store.state.duration.milliseconds;
                 let startTime = moment(new Date()).toISOString();
+                this.startTime = moment(new Date()).toString();
                 
                 let newInterval = setInterval(function() {
                     let nowTime = moment(new Date());
                     let duration = moment.duration(moment(nowTime).diff(startTime));
                     let milliseconds = duration.asMilliseconds();
                     store.setDuration(startMilliseconds + milliseconds);
+                    this.totalTime = duration.format('hh:mm:ss');
                 }, 100);
                 store.setCustomInterval(newInterval, 'chronometer');
             }
@@ -44,9 +49,13 @@ export default {
             let interval = store.getCustomInterval('chronometer');
             clearInterval(interval);
             store.setCustomInterval(false, 'chronometer');
+            this.stopTime = moment(new Date()).toString();
         },
         resetTimer: function () {
             store.setDuration(0);
+            this.startTime = '-';
+            this.stopTime = '-';
+            this.totalTime = '-';
         }
     }
 };
